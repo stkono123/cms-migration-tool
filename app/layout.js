ab95d42b9d5ae143a94468a657f291697cf6e994
stkono123 <stefan.konopatzki@googlemail.com>
2026-04-28 16:19:33 +0200

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'CMS Migration Tool',
+  description: 'AI-powered Shopify to Contentful migration',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="de">
+      <body>{children}</body>
+    </html>
+  )
+}
